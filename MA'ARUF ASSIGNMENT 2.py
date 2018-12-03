be31c3cb1c79c03fe90d2e54a96d5706d0bc8169
SirMurphy <45232486+SirMurphy@users.noreply.github.com>
2018-12-03 13:12:22 +0100

    Add files via upload

---
+++ MA'ARUF ASSIGNMENT 2.py
@@ -0,0 +1,16 @@
+x= 1
+while (x<=100):
+  print(x)
+  x+=1
+  
+  if (x%3==0 and x%5==0):
+      print (x, "fizzbuzz")
+      
+  elif (x%5==0):
+      print (x, "buzz")
+     
+  elif (x%2==0):
+     print (x, "fizz")
+
+
+   
